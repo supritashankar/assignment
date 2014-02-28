227de5597c7ff32a42fc49e09c34a89d51a558e0
Suprita Shankar <suprita.shankar@gmail.com>
2014-02-28 15:46:41 -0500

    ui for results

--- project/clientui/views.py
+++ project/clientui/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render_to_response
 
 def results(request):
   print 'I am here'
+  return render_to_response('clientui/results.html', locals())
