8d4b6a1f9dc2109ab653cf9f8610db6f349629f5
Suprita Shankar <suprita.shankar@gmail.com>
2014-02-28 16:36:19 -0500

    send requests and give to template

--- project/clientui/views.py
+++ project/clientui/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+import json
+
+import requests
 
 def results(request):
-  print 'I am here'
+  api_url = "http://127.0.0.1:8000/api/color/?format=json"
+  results = requests.get(api_url)
+  resp = json.loads(results.content)
+  colors = resp["objects"]
   return render_to_response('clientui/results.html', locals())
