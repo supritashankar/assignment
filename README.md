6354e387a6de87a46f12d85f39dbef80f69f2477
Suprita Shankar <suprita.shankar@gmail.com>
2014-02-28 17:09:31 -0500

    use authorization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 assignment
 ==========
+
+To update data:
+curl --dump-header - -H "Content-Type: application/json" -X PUT --data '{"color": "R"}' http://127.0.0.1:8000/api/color/2/\?format\=json
--- project/portfolio/api.py
+++ project/portfolio/api.py
@@ -1,9 +1,11 @@
 # myapp/api.py
 from tastypie.resources import ModelResource
 from portfolio.models import Color
-
+from tastypie.authorization import Authorization
 
 class ColorResource(ModelResource):
     class Meta:
         queryset = Color.objects.all()
         resource_name = 'color'
+	authorization = Authorization()
+	allowed_methods = ['get', 'put']
